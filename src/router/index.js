671d28a0406c47291c3addb14de93a05d6de3912
Ankit Srivastava <ankitsrivas14@gmail.com>
2022-03-16 00:47:37 +0530

    basic clean up

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LandingPage from '../views/LandingPage.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'landing-page',
+    component: LandingPage
   },
   {
     path: '/about',
