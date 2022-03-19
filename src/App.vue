92954cb6db9e32139df27dc49cc0123638e465d9
Ankit Srivastava <ankitsrivas14@gmail.com>
2022-03-19 15:54:10 +0530

    showing under upgrade

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
-    <NavigationBar />
-    <router-view/>
+    <!-- <NavigationBar />
+    <router-view/> -->
+    <div class="pf-under-upgrade">
+        <div>Under Maintenance</div>
+        <div class="subtext">Contact at hi@ankita.design</div>
+    </div>
   </div>
 </template>
 <script>
@@ -13,4 +17,19 @@ export default {
 }
 </script>
 <style lang="scss">
+.pf-under-upgrade{
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  font-weight: 700;
+  font-size: 42px;
+  .subtext{
+    font-size: 28px;
+    font-weight: 500;
+    margin-top: 20px;
+  }
+}
 </style>
